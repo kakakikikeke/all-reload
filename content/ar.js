bf2eff5ac5c16d02354f8971d0de681703f910fd
kakakikikeke <cdl.yoshinaka@gmail.com>
2012-07-08 21:25:17 +0900

    03_comment fix miss alert

--- content/ar.js
+++ content/ar.js
@@ -1,6 +1,5 @@
 function reloadAllTabs () {
 	var browser = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
 	var mw = browser.getMostRecentWindow("navigator:browser");
-	window.alert("test");
 	mw.gBrowser.reloadAllTabs();
 };
