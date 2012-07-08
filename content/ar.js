df59aca172b428b4e56a305d0250243df925eeb9
kakakikikeke <cdl.yoshinaka@gmail.com>
2012-07-08 20:45:20 +0900

    01_comment fix 100%

--- content/ar.js
+++ content/ar.js
@@ -1,3 +1,6 @@
-function reloadAllTabs() {
-	
-}
+function reloadAllTabs () {
+	var browser = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+	var mw = browser.getMostRecentWindow("navigator:browser");
+	window.alert("test");
+	mw.gBrowser.reloadAllTabs();
+};
