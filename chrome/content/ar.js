c5ef429098b00314e61da102cced47991ac84eb5
kakakikikeke <cdl.yoshinaka@gmail.com>
2018-09-07 14:27:45 +0900

    Remove old files

--- chrome/content/ar.js
+++
@@ -1,5 +0,0 @@
-function reloadAllTabs () {
-	var browser = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
-	var mw = browser.getMostRecentWindow("navigator:browser");
-	mw.gBrowser.reloadAllTabs();
-};
